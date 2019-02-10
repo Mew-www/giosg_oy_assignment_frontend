fd5000c9d30d4b12e3731f761aaec418e4255889
Mew-www <mew.proxy@hotmail.com>
2019-02-10 17:19:23 +0200

    create-react-app & component breakdown in App.js

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  componentDidMount() {
+    // Load start_date, end_date, access_token
+  }
+  render() {
+    return (
+      <div>
+        <header>
+          Backend test UI
+        </header>
+        <div>
+          controls
+        </div>
+        <div>
+          totals
+        </div>
+        <div>
+          <div>
+            daily table
+          </div>
+          <div>
+            table controls (page)
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
