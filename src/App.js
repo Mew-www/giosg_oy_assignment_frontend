6bc890deeacae507cc159bd8ae4e16ec58152b1d
Mew-www <mew.proxy@hotmail.com>
2019-02-10 19:41:45 +0200

    Implement GET'ting total numbers, API_ROOT inn .env

--- src/App.js
+++ src/App.js
@@ -4,13 +4,62 @@ import './scss-common/MediaObject.scss';
 import './App.scss';
 import account_img from './account.svg'
 import DataPanel from "./subcomponents/DataPanel";
+import axios from 'axios';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-
+      start_date: '',
+      end_date: '',
+      access_token: '',
+      error: '',
+      total_numbers: null,
+      daily_numbers: null
     };
+    this.changeStart = this.changeStart.bind(this);
+    this.changeEnd = this.changeEnd.bind(this);
+    this.changeToken = this.changeToken.bind(this);
+    this.submitQuery = this.submitQuery.bind(this);
+  }
+  changeStart(event) {
+    this.setState({start_date: event.target.value});
+  }
+  changeEnd(event) {
+    this.setState({end_date: event.target.value});
+  }
+  changeToken(event) {
+    this.setState({access_token: event.target.value});
+  }
+  submitQuery(event) {
+    if (this.state.start_date.trim().length === 0) {
+      this.setState({error: 'Start date is required field'});
+    } else if (this.state.end_date.trim().length === 0) {
+      this.setState({error: 'End date is required field'});
+    } else if (this.state.access_token.trim().length === 0) {
+      this.setState({error: 'Access token is required field'});
+    } else {
+      this.setState({error: ''});
+      axios.get(
+        `${process.env.REACT_APP_API_ROOT}/reporting/v1/total/from-${this.state.start_date}/to-${this.state.end_date}/`,
+        {headers: {'X-Gi-Token': this.state.access_token}}
+      )
+        .then((response) => {
+          this.setState({total_numbers: response.data});
+        })
+        .catch((error) => {
+          if (error.response.status === 404) {
+            this.setState({error: 'Invalid start- or end-date'});
+          } else if (error.response) {
+            this.setState({error: error.response.data});
+          } else if (error.request) {
+            this.setState({error: error.request});
+          } else {
+            this.setState({error: error.message});
+          }
+        })
+    }
+    event.preventDefault();
   }
   componentDidMount() {
     // Load start_date, end_date, access_token
@@ -24,7 +73,7 @@ class App extends Component {
         </header>
 
         {/* Main controls, to query API */}
-        <form className="App__controls" onSubmit={(event) => {event.preventDefault();}}>
+        <form className="App__controls" onSubmit={this.submitQuery}>
           <div className="App__controls-options">
             <div className="DateControl">
               <label>
@@ -32,6 +81,7 @@ class App extends Component {
                 <br/>
                 <input className="DateControl__input"
                        type="text"
+                       onChange={this.changeStart}
                        placeholder="YYYY-MM-DD"
                 />
               </label>
@@ -42,6 +92,7 @@ class App extends Component {
                 <br/>
                 <input className="DateControl__input"
                        type="text"
+                       onChange={this.changeEnd}
                        placeholder="YYYY-MM-DD"
                 />
               </label>
@@ -55,6 +106,7 @@ class App extends Component {
               />
               <input className="MediaObject__content"
                      type="text"
+                     onChange={this.changeToken}
                      placeholder="Access token"
               />
             </div>
@@ -68,33 +120,45 @@ class App extends Component {
         </form>
 
         {/* Errors (if any) */}
-        <p className="App__error">Errors</p>
+        {this.state.error ?
+          <p className="App__error">{this.state.error}</p>
+          :
+          ""
+        }
 
         {/* Printout from the API result (if any) */}
-        <div className="App__total-numbers">
-          <DataPanel
-            name="Total conversation count"
-            count={0}
-          />
-          <DataPanel
-            name="Total user message count"
-            count={0}
-          />
-          <DataPanel
-            name="Total visitor messages count"
-            count={0}
-          />
-        </div>
+        {this.state.total_numbers ?
+          <div className="App__total-numbers">
+            <DataPanel
+              name="Total conversation count"
+              count={this.state.total_numbers.total_conversation_count}
+            />
+            <DataPanel
+              name="Total user message count"
+              count={this.state.total_numbers.total_user_message_count}
+            />
+            <DataPanel
+              name="Total visitor messages count"
+              count={this.state.total_numbers.total_visitor_message_count}
+            />
+          </div>
+          :
+          ""
+        }
 
         {/* Detailed printout with its own sub-controls*/}
-        <div className="App__daily-numbers">
-          <div>
-            daily table
+        {this.state.daily_numbers ?
+          <div className="App__daily-numbers">
+            <div>
+              daily table
+            </div>
+            <div>
+              table controls (page)
+            </div>
           </div>
-          <div>
-            table controls (page)
-          </div>
-        </div>
+          :
+          ""
+        }
       </div>
     );
   }
